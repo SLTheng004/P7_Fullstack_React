55fe1d3a1d226fb4c8528c15c9f82a5649bc6d29
Samantha Theng <Samanthaa.LT.004@gmail.com>
2023-07-21 19:08:54 -0500

    added multer to server

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const { Posts, Likes } = require('../models')
 const { validateToken } = require('../middleware/Auth');
-
+const { multer } = require('../middleware/multer');
 
 router.get('/', validateToken, async (req, res) => {
-    const listOfPosts = await Posts.findAll({ include: [Likes] });
+    const listOfPosts = await Posts.findAll({ include: [Likes] })
     const likedPosts = await Likes.findAll({where: {UserId: req.user.id}})
     res.json({listOfPosts: listOfPosts, likedPosts: likedPosts});
 });
@@ -14,11 +14,13 @@ router.get('/byId/:id', async (req, res) => {
     const id = req.params.id
     const post = await Posts.findByPk(id);
     res.json(post);
-})
+});
 
-router.post('/', validateToken, async (req, res) => {
-    const post = req.body;
-    post.username = req.user.username; 
+router.post('/', validateToken, multer, async (req, res) => {
+    const post = ({
+        imageUrl: url + '/images/' + req.file.filename,
+        username: req.user.username,
+    })
     await Posts.create(post);
     res.json(post);
 });
