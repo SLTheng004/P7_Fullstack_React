56d98effdca86b0894533a19e5ad65a26e3811a3
Samantha Theng <Samanthaa.LT.004@gmail.com>
2023-08-01 18:59:45 -0500

    add setAuthState for delete user

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,24 +5,41 @@ import axios from 'axios';
 import '../css/Profile.css';
 
 function Profile({ logout }) {
-
-  const { authState } = useContext(AuthContext);
+  const { authState, setAuthState } = useContext(AuthContext);
   let navigate = useNavigate();
 
   //check if auth, else send to login
   useEffect(() => {
     if (!localStorage.getItem("accessToken")) {
       navigate('/login');
+    } else {
+      axios.get('http://localhost:4000/auth/user', {
+                headers: { accessToken: localStorage.getItem('accessToken') }
+    }).then((response) => [
+      setAuthState({
+        username: response.data.username,
+        id: response.data.id,
+        status: true,
+      })
+    ])
     }
   }, []);
 
   //delete user
   const onDelete = () => {
     axios.delete('http://localhost:4000/auth/user/' + authState.id,
-      { headers: { accessToken: localStorage.getItem('accessToken') } }).then(() => {
+      {
+        headers: { accessToken: localStorage.getItem('accessToken') }
+      }).then((response) => {
+        setAuthState({
+          username: response.data.username,
+          id: response.data.id,
+          status: false,
+        })
         logout();
         navigate('/login');
-      })
+        alert('Account Deleted!')
+      });
   }
 
   return (
